54cad4d899563589419988dbe9b441fcb0ca0879
smithpiano <learn.co@smithpiano.com>
2015-12-05 18:18:06 -0500

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,23 @@
-# code your #position_taken? method here!
+
+
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def move(board, position, value = "X")
+  board[(position.to_i)-1] = value
+end
+
+
+def position_taken?(board, position)
+  if (board[position] == "X") || (board[position] == "O")
+    true
+  elsif board[position] == " " || ""
+    false
+  end
+end
